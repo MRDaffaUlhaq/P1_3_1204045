493fa8460a500886b5ee13a9fda2f8f463b36d44
daffaulhaq <udaffa09@gmail.com>
2021-11-12 21:39:38 +0700

    Memperbaiki validasi dan logika pengulangan yang disesuaikan dengan input

--- P1_3_1204045/Program.cs
+++ P1_3_1204045/Program.cs
@@ -17,14 +17,87 @@ namespace P1_4_1204045
             int ayam = Convert.ToInt16(Console.ReadLine());
 
             Console.WriteLine("");
-            if (ayam == 10)
+            if (ayam >= 1 && ayam <= 10 )
             {
-                for (int i = 10; i > 1; i--)
+                if (ayam == 10)
                 {
-                    Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    for (int i = 10; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }else if (ayam == 9)
+                {
+                    for (int i = 9; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
                 }
-                Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
-
+                else if (ayam == 8)
+                {
+                    for (int i = 8; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+                else if (ayam == 7)
+                {
+                    for (int i = 7; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+                else if (ayam == 6)
+                {
+                    for (int i = 6; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+                else if (ayam == 5)
+                {
+                    for (int i = 5; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+                else if (ayam == 4)
+                {
+                    for (int i = 4; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+                else if (ayam == 3)
+                {
+                    for (int i = 3; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+                else if (ayam == 2)
+                {
+                    for (int i = 2; i > 1; i--)
+                    {
+                        Console.WriteLine("Anak ayam turunlah " + i + " mati satu tinggallah " + (i - 1));
+                    }
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+                else if (ayam == 1)
+                {
+                    Console.WriteLine("Anak ayam turunlah " + 1 + " mati satu tinggallah induknya");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ups, Nilai input harus bernilai positif, tidak boleh nol dan kurang dari sepuluh");
             }
             
             
